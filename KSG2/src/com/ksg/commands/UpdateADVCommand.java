bd93889310d9b248da80680dc6201a7411bf8138
박창현 <@DESKTOP-K240QPK>
2021-02-24 22:54:58 +0900

    설정변경

--- KSG2/src/com/ksg/commands/UpdateADVCommand.java
+++
@@ -1,9 +0,0 @@
-package com.ksg.commands;
-
-public class UpdateADVCommand implements KSGCommand{
-
-	public int execute() {
-		return 0;
-	}
-
-}
--- KSG2/src/com/ksg/view/base/TableListener.java
+++
@@ -1,7 +0,0 @@
-package com.ksg.view.base;
-
-public interface TableListener {
-	
-	public void updateTable();
-
-}
