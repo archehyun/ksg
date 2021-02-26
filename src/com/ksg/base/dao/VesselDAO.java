1925d77b6bba3abec91fdb2239eb13bc01bd3162
박창현 <@DESKTOP-K240QPK>
2021-02-26 10:05:25 +0900

    VesselDAO 메소드 추가

--- src/com/ksg/base/dao/VesselDAO.java
+++ src/com/ksg/base/dao/VesselDAO.java
@@ -1,5 +1,9 @@
 package com.ksg.base.dao;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
 import com.ksg.common.dao.AbstractDAO;
 
 public class VesselDAO extends AbstractDAO{
@@ -8,6 +12,12 @@ public class VesselDAO extends AbstractDAO{
 	 */
 	public VesselDAO() {
 		super();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> selectVesselList(Map<String, Object> commandMap) throws SQLException {
+		return selectList("vessel.selectVesselList", commandMap);
+
 	}
 
 }
