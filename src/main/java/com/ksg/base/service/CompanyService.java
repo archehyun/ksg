c44ad26c02253065418ce9cdf33ef6aa3d87c810
pch <pch@DESKTOP-0SQQKAG>
2021-07-08 18:58:39 +0900

    로그 업데이트

--- src/main/java/com/ksg/base/service/CompanyService.java
+++ src/main/java/com/ksg/base/service/CompanyService.java
@@ -4,9 +4,15 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.ksg.base.dao.CompanyDAO;
 
 public class CompanyService {
+	
+	
+	private static final Logger logger = LoggerFactory.getLogger(CompanyService.class);
 	
 	
 	CompanyDAO companyDAO;
@@ -15,30 +21,34 @@ public class CompanyService {
 	}
 	
 	@SuppressWarnings("unchecked")
-	public Map<String, Object> selectCompanyList(Map<String, Object> commandMap) throws SQLException {
+	public Map<String, Object> selectCompanyList(Map<String, Object> param) throws SQLException {
+		
+		
+		logger.info("param:{}",param);
 		
 		HashMap<String, Object> resultMap = new HashMap<String, Object>();
 		
-		resultMap.put("total", companyDAO.selectCompanyCount(commandMap));
+		resultMap.put("total", companyDAO.selectCompanyCount(param));
 		
-		resultMap.put("master", companyDAO.selectCompanyList(commandMap));
+		resultMap.put("master", companyDAO.selectCompanyList(param));
 		
 		return resultMap;
 
 	}
 
 	public int updateComapny(HashMap<String, Object> param) throws SQLException{
+		logger.info("param:{}",param);
 		return companyDAO.updateCompany(param);
 		
 	}
 
 	public int deleteCompany(HashMap<String, Object> param) throws SQLException{
-		// TODO Auto-generated method stub
+		logger.info("param:{}",param);
 		return companyDAO.deleteCompany(param);
 	}
 
 	public void insertComapny(HashMap<String, Object> param) throws SQLException{
-		// TODO Auto-generated method stub
+		logger.info("param:{}",param);
 		
 		companyDAO.insertCompany(param);
 	}
--- src/main/java/com/ksg/base/service/VesselService.java
+++ src/main/java/com/ksg/base/service/VesselService.java
@@ -2,56 +2,67 @@ package com.ksg.base.service;
 
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.ksg.base.dao.VesselDAO;
 
 public class VesselService {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CompanyService.class);
+	
 	VesselDAO vesselDAO;
+	
 	public VesselService() {
 		
 		vesselDAO = new VesselDAO();
 	}
 	
 	@SuppressWarnings("unchecked")
-	public Map<String, Object> selectVesselList(Map<String, Object> commandMap) throws SQLException {
+	public Map<String, Object> selectVesselList(Map<String, Object> param) throws SQLException {
+		
+		logger.info("param:{}",param);
 		
 		HashMap<String, Object> resultMap = new HashMap<String, Object>();
 		
-		resultMap.put("total", vesselDAO.selectVesselCount(commandMap));
+		resultMap.put("total", vesselDAO.selectVesselCount(param));
 		
-		resultMap.put("master",vesselDAO.selectVesselList(commandMap));
+		resultMap.put("master",vesselDAO.selectVesselList(param));
 		
 		return resultMap;
 
 	}
 
-	public Map<String, Object> selectVesselAbbrList(HashMap<String, Object> commandMap) throws SQLException {
+	public Map<String, Object> selectVesselAbbrList(HashMap<String, Object> param) throws SQLException {
 		HashMap<String, Object> resultMap = new HashMap<String, Object>();
 		
-		//resultMap.put("total", vesselDAO.selectVesselCount(commandMap));
-		
-		resultMap.put("master",vesselDAO.selectVesselAbbrList(commandMap));
+		logger.info("param:{}",param);		
+		resultMap.put("master",vesselDAO.selectVesselAbbrList(param));
 		return resultMap;
 	}
 
 	public Object updateVesselAbbr(HashMap<String, Object> param) throws SQLException {
+		logger.info("param:{}",param);
 		return vesselDAO.updateVesselAbbr(param);
 		
 	}
 
-	public Object deleteVessel(HashMap<String, Object> pram) throws SQLException {
-		return vesselDAO.deleteVessel(pram);
+	public Object deleteVessel(HashMap<String, Object> param) throws SQLException {
+		logger.info("param:{}",param);
+		return vesselDAO.deleteVessel(param);
 		
 	}
 
-	public void insertVessel(HashMap<String, Object> info) throws SQLException {
-		vesselDAO.insertVessel(info);
+	public void insertVessel(HashMap<String, Object> param) throws SQLException {
+		logger.info("param:{}",param);
+		vesselDAO.insertVessel(param);
 		
 	}
 
 	public void deleteVesselAbbr(HashMap<String, Object> param) throws SQLException {
+		logger.info("param:{}",param);
 		vesselDAO.deleteVesselAbbr(param);
 		
 	}
