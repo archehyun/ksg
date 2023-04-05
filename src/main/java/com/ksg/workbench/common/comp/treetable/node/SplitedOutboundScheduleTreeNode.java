301b45a19b2929af080822d4f8ce87edff052f68
archehyun <archehyun@naver.com>
2023-04-05 16:47:52 +0900

    update

---
+++ src/main/java/com/ksg/workbench/common/comp/treetable/node/SplitedOutboundScheduleTreeNode.java
@@ -0,0 +1,9 @@
+package com.ksg.workbench.common.comp.treetable.node;
+
+public class SplitedOutboundScheduleTreeNode extends ScheduleTreeNode{
+	public SplitedOutboundScheduleTreeNode(String format) {
+		super(format, NodeType.SPLITED_SCHEDULE);
+	}
+
+
+}
